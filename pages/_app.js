e831e992cf07e6419001049184683486f553f767
ChristophObst <christoph.obst@posteo.de>
2023-05-24 11:39:56 +0200

    test1

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import GlobalStyle from "../styles";
 
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
-//
+//--
 export default function App({ Component, pageProps }) {
   return (
     <>
