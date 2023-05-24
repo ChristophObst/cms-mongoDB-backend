134eaabedab44a94bebb007ddd95c132e74a8566
ChristophObst <christoph.obst@posteo.de>
2023-05-24 12:52:25 +0200

    cms-migriert

--- styles.js
+++ styles.js
@@ -6,22 +6,16 @@ export default createGlobalStyle`
     --color-granite: #252629;
     --color-water-10: #f3f5f9;
     --color-foam: #ffffff;
-
-    --gap: 20px;
+   
   }
   
   *,
   *::before,
   *::after {
-    box-sizing: border-box;
+   
   }
 
-  body {
-    display: grid;
-    margin: auto;
-    font-family: system-ui;
-    place-items: center;
-    min-height: 100vh;
-    max-width: 50rem;
+  body { 
+    width: 100%;
   }
 `;
