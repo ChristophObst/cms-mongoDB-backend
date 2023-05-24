02aa9f014197417904ce9fc4dd3953a390e9d45d
ChristophObst <christoph.obst@posteo.de>
2023-05-25 00:00:58 +0200

    migration

--- db/models/Product.js
+++ db/models/Product.js
@@ -5,10 +5,10 @@ const { Schema } = mongoose;
 
 const productSchema = new Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
+  /*  description: { type: String, required: true },
   price: { type: Number, required: true },
   currency: { type: String, required: true },
-  reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
+  reviews: { type: [Schema.Types.ObjectId], ref: "Review" }, */
 });
 
 const Product =
